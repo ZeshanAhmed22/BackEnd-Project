fd4076c24ef0924a7842b52659b8d6ad4d087f26
ZeshanAhmed <ahmedzeshan22@gmail.com>
2022-08-13 19:44:58 +0100

    implemented Post method and errors

--- controllers/commentsById.js
+++ controllers/commentsById.js
@@ -1,10 +1,31 @@
-const { fetchCommentsByArticleId } = require("../models/commentsById");
+const { fetchArticlesById } = require("../models/articlesById");
+const {
+  fetchCommentsByArticleId,
+  insertComments,
+} = require("../models/commentsById");
 
 exports.getCommentsByArticleId = (request, response, next) => {
   const { id } = request.params;
 
   fetchCommentsByArticleId(id).then((comments) => {
-    console.log(comments);
     response.status(200).send({ comments });
   });
+};
+
+exports.postComments = (request, response, next) => {
+  console.log("in the controller");
+  const { id } = request.params;
+  console.log(request.body);
+
+  fetchArticlesById(id)
+    .then((article) => {
+      console.log(article);
+
+      insertComments(request.body, id).then((comments) => {
+        response.status(201).send(comments);
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
