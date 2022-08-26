9bd4698d547aba03eab85136358c166858a2ed7a
ZeshanAhmed <ahmedzeshan22@gmail.com>
2022-08-26 09:41:37 +0100

    add error handling 23502

--- errors/errors.js
+++ errors/errors.js
@@ -4,7 +4,7 @@ exports.handleCustomErrors = (err, request, response, next) => {
   } else next(err);
 };
 exports.handlePsqlError = (err, request, response, next) => {
-  if (err.code === "22P02") {
+  if (err.code === "22P02" || err.code === "23502") {
     response.status(400).send({ msg: "Invalid input" });
   } else next(err);
 };
